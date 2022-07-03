b8c8eda5ab13c01aabb0c3ed0cebaf37712572d6
TariqueNasar <nasartarique@gmail.com>
2022-07-04 00:48:17 +0530

    azureclient changes

--- app.py
+++ app.py
@@ -7,23 +7,22 @@ app = Flask(__name__)
 def get_file():
     filename = request.args.get('filename')
     az = AzureClient()
-    blob = az.azread(filename)
+    blob = az.read(filename)
     return blob
-    
 
 @app.route("/",methods=['POST'])
 def post_file():
-    filename = request.data['filename']
-    blob = request.data['filename']
+    azure = AzureClient()
+    if 'file' not in request.files:
+        return "No files uploaded"
+    file = request.files['file']
+    filename = file.filename
+    azure.upload(file,filename)
+    return f"file {filename} uploaded"
 
 # test blobs
 @app.route("/list",methods=['GET'])
 def list_blobs():
     az = AzureClient()
-    blobs = az.azlist()
-    return ''.join([blob.name for blob in blobs])
-    
-
-
-
-
+    blobs = az.listfiles()
+    return ''.join([blob.name+'\n' for blob in blobs])
--- azblob/azureclient.py
+++ azblob/azureclient.py
@@ -1,4 +1,4 @@
-from azure.storage.blob import BlobServiceClient ,ContainerClient 
+from azure.storage.blob import BlobServiceClient ,ContainerClient
 import os
 
 os.environ['AZURE_STORAGE_CONNECTION_STRING'] = 'DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;'
@@ -9,25 +9,32 @@ CONTAINER_NAME = os.environ.get("STORAGE_CONTAINER")
 class AzureClient:
     def __init__(self):
         self.blob_service_client = BlobServiceClient.from_connection_string(CONNECTION_STRING)
+
+    def create_connection_string(self,accountname,accountkey):
+        pass
     
     def check_auth(self):
         pass
+    
+    def createcontainer(self,container):
+        container_client = ContainerClient(os.environ.get("AZURE_STORAGE_CONNECTION_STRING"),container_name=container)
+        container_client.create_container()
 
-    def azupload(self,fileblob,filename):
+    def upload(self,fileblob,filename):
         blob_client = self.blob_service_client.get_blob_client(CONTAINER_NAME,blob=filename)
         blob_client.upload_blob(fileblob)
 
-    def azread(self,filename):
+    def read(self,filename):
         blob_client = self.blob_service_client.get_blob_client(CONTAINER_NAME,blob=filename)
         blob =blob_client.download_blob()
         url  = blob_client.url
         return url
     
-    def azdelete(self,filename):
+    def delete(self,filename):
         blob_client = self.blob_service_client.get_blob_client(CONTAINER_NAME,blob=filename)
         blob_client.delete_blob()
 
-    def azlist(self):
+    def listfiles(self):
         container = ContainerClient.from_connection_string(conn_str=CONNECTION_STRING, container_name=CONTAINER_NAME)
         blob_list = container.list_blobs()
         return blob_list
